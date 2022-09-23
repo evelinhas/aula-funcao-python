3a9bc77d42b53c7603a61f39918838a276c35e2b
Evelin <evelin.has@gmail.com>
2022-09-22 22:10:05 -0300

    funcao exibir nome

---
+++ script/Evelin_Haslinger/script-funcao.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+"""script somatoria"""
+
+__version__="0.0.1"
+__author__= "Evelin"
+__license__="GNU v3"
+
+def somatoria(valor_x,valor_y):
+    return valor_x + valor_y
+
+result= somatoria(7,8)
+print(result)
--- script/script-funcao.py
+++ script/script-funcao.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
-"""descrição
+"""função exibir nome 
 """
 __version__ = "0.0.1"
-__author__ = "Daviny Vidal"
+__author__ = "Evelin Haslinger"
 __license__ = "GNU v3"
 
+from unicodedata import name
+from xml.sax.handler import property_interning_dict
+
+
+def exibir_nome(input_name):
+    print("O nome escolhido foi:") 
+    print(input_name)
+
+
+
+print("Escreva um nome:")
+name=input()
+exibir_nome(name)
